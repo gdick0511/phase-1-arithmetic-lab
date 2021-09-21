7f632c79905a686c6e84d4e0beefaaf4ec656dd7
garrettdick <garrettd0511@gmail.com>
2021-09-20 21:50:26 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const num3 = 29
+const num4 = 5
+
+let multiply = (num1*num2);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = (num3%num4);
+
+let max = Math.max(1,5,9,10,7,20,3);
